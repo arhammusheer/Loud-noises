8e73645d5b4959fc64c0e31bdacb600a28bb9490
arhammusheer <arhammusheer@gmail.com>
2021-05-19 02:51:55 +0530

    added selection support

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -4,15 +4,34 @@ let messageBox = document.getElementById("message-box");
 let messageBtn = document.getElementById("message-button");
 let messageLog = document.getElementById("message-log");
 let bigRedButton = document.getElementById("big-red-button");
+let audioList = document.getElementById("audio-list");
 
 let interaction = false;
-let loudNoises = new Audio();
+let audioPlayer = new Audio();
+
+audios = {
+  "bitch-gimme-attention": "/audio/bitch-gimme-attention.mp3",
+  bruh: "/audio/bruh.mp3",
+  "discord-notification": "/audio/discord-notification.mp3",
+  "loud-noises": "/audio/loud-noises.mp3",
+  sheeesh: "/audio/sheeesh.mp3",
+  "where-is-my-supersuit": "/audio/where-is-my-supersuit.mp3",
+  "why-are-you-runnin": "/audio/why-are-you-runnin.mp3",
+  "why-you-bully-me": "/audio/why-you-bully-me.mp3",
+};
+
+for (audio in audios) {
+  newAudio = document.createElement("option");
+  newAudio.value = audios[audio];
+  newAudio.innerHTML = audio;
+  audioList.appendChild(newAudio);
+}
 
 window.onclick = () => {
-  if (!interaction) loudNoises.play();
+  if (!interaction) audioPlayer.play();
   interaction = true;
-  loudNoises.src = "/audio/loud-noises.mp3";
-  bigRedButton.innerHTML = "BIG RED BUTTON"
+  audioPlayer.src = audioList.options[audioList.selectedIndex].value;
+  bigRedButton.innerHTML = "BIG RED BUTTON";
 };
 
 const socket = io();
@@ -27,17 +46,18 @@ socket.on("text message", (msg) => {
   console.log(msg);
 });
 
-loudNoises.addEventListener("canplay", () => {
+audioPlayer.addEventListener("canplay", () => {
   bigRedButton.disabled = false;
 });
 
-socket.on("big red button", (button1) => {
-  if (button1) {
+socket.on("big red button", (data) => {
+  if (data) {
     let newMessage = document.createElement("div");
     newMessage.innerHTML = `<span class="badge bg-danger m-1" style="border-radius:1rem">THE BIG RED BUTTON WAS PRESSED</span>`;
     messageLog.appendChild(newMessage);
     messageLog.scrollTop = messageLog.scrollHeight;
-    loudNoises.play();
+    audioPlayer.src = data.href;
+    audioPlayer.play();
   }
 });
 
@@ -54,7 +74,9 @@ messageBox.addEventListener("keyup", (e) => {
 });
 
 bigRedButton.onclick = () => {
-  socket.emit("big red button", true);
+  socket.emit("big red button", {
+    href: audioList.options[audioList.selectedIndex].value,
+  });
   let newMessage = document.createElement("div");
   newMessage.innerHTML = `<span class="badge bg-primary m-1" style="border-radius:1rem">YOU PRESSED THE BIG RED BUTTON</span>`;
   messageLog.appendChild(newMessage);
