0a1a5ef5fdeea3823097ad2c3c08e70b1aafa89e
arhammusheer <arhammusheer@gmail.com>
2021-06-19 19:42:31 +0530

    auto loader

--- socketapi.js
+++ socketapi.js
@@ -3,35 +3,24 @@ const socketapi = {
   io: io,
 };
 
-const audios = {
-  "anime-wow": "/audio/anime-wow.mp3",
-  "bitch-gimme-attention": "/audio/bitch-gimme-attention.mp3",
-  bonk: "/audio/bonk.mp3",
-  bruh: "/audio/bruh.mp3",
-  "discord-notification": "/audio/discord-notification.mp3",
-  "epic-choir": "/audio/epic-choir.mp3",
-  "elevator-music": "/audio/elevator-music.mp3",
-  "goat-scream": "/audio/goat-scream.mp3",
-  "heavenly-music": "/audio/heavenly-music.mp3",
-  "i-lo-youu": "/audio/i-lo-youu.mp3",
-  "loud-noises": "/audio/loud-noises.mp3",
-  "love-moment": "/audio/love-moment.mp3",
-  naniii: "/audio/naniii.mp3",
-  rickroll: "/audio/rickroll.mp3",
-  sheeesh: "/audio/sheeesh.mp3",
-  "where-is-my-supersuit": "/audio/where-is-my-supersuit.mp3",
-  "why-are-you-runnin": "/audio/why-are-you-runnin.mp3",
-  "why-you-bully-me": "/audio/why-you-bully-me.mp3",
-  yeet: "/audio/yeet.mp3",
-  "you-know": "/audio/you-know.mp3",
-};
+const fs = require("fs");
+const path = require("path");
+
+const sounds = fs.readdirSync(path.join(__dirname, "public/audio"));
+
+const audios = {};
+
+for (sound of sounds) {
+  audio_path = path.parse(path.join(__dirname, `public/audio/${sound}`));
+  if (sound !== ".DS_Store") audios[audio_path.name] = sound;
+}
 
 
 // Add your socket.io logic here!
 io.on("connection", function (socket) {
   console.log("A user connected");
 
-  socket.emit("audios", audios)
+  socket.emit("audios", audios);
 
   socket.on("room", function (room) {
     socket.join(room);
